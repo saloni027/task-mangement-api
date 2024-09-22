63f0fc1146a43520e3a9accf848cec584a8b8bc8
Saloni Sinha <38814940+saloni027@users.noreply.github.com>
2024-09-22 17:25:54 +0000

    Save

---
+++ src/auth/token_handler.py
@@ -0,0 +1,20 @@
+from jose import JWTError, jwt
+from fastapi import Depends,HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+
+SECRET_KEY = 'secret'
+ALGORITHM = "HS256"
+scheme = OAuth2PasswordBearer(tokenUrl="token")
+token_depends = Annotated[str,Depends(scheme)]
+
+def handle_token(token: token_depends):
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+verify_token = Annotated[str, Depends(handle_token)]
--- src/routes/update_task.py
+++ src/routes/update_task.py
@@ -3,9 +3,10 @@ from src.database.models import Tasks
 from src.database.config import db_dependency
 from src.routes.task_router import task_router
 from fastapi import status
+from src.auth.token_handler import verify_token
 
 @task_router.put(path="/{task_id}", response_model=TaskResponse, status_code=status.HTTP_200_OK)
-def update_task(task_id: int, task: TaskUpdate, db: db_dependency):
+def update_task(task_id: int, task: TaskUpdate, db: db_dependency, token:verify_token ):
     db_task = db.query(Tasks).filter(Tasks.id==task_id).first()
     if not task:
         raise HTTPException(status_code=404, detail=f"Task with id {task_id} not found")
